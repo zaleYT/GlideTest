6a0021717a976d85255759d73003b7ebd201cbd6
yutao <register_yu@163.com>
2018-08-11 14:33:49 +0800

    Glid test with recyclerview

---
+++ app/src/main/java/com/travis/glidetest/MainActivity.java
@@ -0,0 +1,137 @@
+package com.travis.glidetest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    private RecyclerView recyclerView;
+
+    private List<String> list = new ArrayList<String>();
+
+    private RecyclerAdapter adapter;
+
+    private boolean isInited = false;
+
+    public static final int INDEX_IMAGE_ID = 0;
+    public static final int INDEX_IMAGE_PATH = 1;
+    public static final int INDEX_IMAGE_SIZE = 2;
+    public static final int INDEX_IMAGE_DISPLAY_NAME = 3;
+
+    public static final int EXTERNAL_STORAGE_REQ_CODE = 15 ;
+
+    String[] projImage = {
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DATA, // 路径
+            MediaStore.Images.Media.SIZE,
+            MediaStore.Images.Media.DISPLAY_NAME
+    };
+
+    Uri mImangeUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+
+        if (Build.VERSION.SDK_INT > 23){
+            getRuntimePermission();
+        }else {
+            init();
+        }
+    }
+
+    private void init() {
+        initImagePath();
+
+        //recyclerView.addOnScrollListener(new RecyclerScrollListener());
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+
+        adapter = new RecyclerAdapter(this, list, recyclerView);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initImagePath() {
+        list = new ArrayList<String>();
+
+        final Cursor cursor = getContentResolver().query(
+                mImangeUri,
+                projImage,
+                null,
+                null,
+                null
+        );
+
+        if (cursor != null){
+            String path;
+            while (cursor.moveToNext()){
+                path = cursor.getString(INDEX_IMAGE_PATH);
+                list.add(path);
+                //Log.d(TAG, "image, path:" + cursor.getString(INDEX_IMAGE_PATH));
+            }
+        }
+
+        cursor.close();
+    }
+
+    public void loadImage(View view) {
+        String url = "http://p1.pstatp.com/large/166200019850062839d3";
+        Glide.with(this).load(url).into(imageView);
+    }
+
+    private void getRuntimePermission() {
+        // 没有获得权限
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            // 如果APP的权限曾经被用户拒绝过，就需要在这里更用户做出解释
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)){
+                //Toast.makeText(this, "please give me the permission", Toast.LENGTH_LONG).show();
+            }
+            // 请求权限
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    EXTERNAL_STORAGE_REQ_CODE);
+        }else {
+            // 获得了权限
+            init();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == EXTERNAL_STORAGE_REQ_CODE){
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                init();
+            }else{
+                finish();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/travis/glidetest/RecyclerAdapter.java
@@ -0,0 +1,159 @@
+package com.travis.glidetest;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.ArraySet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.DrawableTypeRequest;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.Target;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by yutao on 2018/8/10.
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    public static final String TAG = "RecyclerAdapter";
+
+    private List<String> list;
+    private Context context;
+    private RecyclerView recyclerView;
+
+    //private Set<LoadCacheAsyncTask> loadCacheTask;
+    //private Set<LoadImageAsyncTask> loadImageTasks;
+
+    public RecyclerAdapter(Context context, List<String> list, RecyclerView recyclerView) {
+        this.list = list;
+        this.context = context;
+        this.recyclerView = recyclerView;
+
+        //loadCacheTask = new HashSet<LoadCacheAsyncTask>();
+
+        //loadImageTasks = new HashSet<LoadImageAsyncTask>();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_item, null);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        ImageView imageView = holder.imageView;
+
+        //imageView.setTag(position);
+        //imageView.setImageBitmap(null);
+        //imageView.setBackgroundResource(R.color.colorgray);
+        Log.d(TAG, "positon:" +position);
+        Glide.with(context)
+                .load(list.get(position))
+                .placeholder(R.color.colorgray)
+                .into(imageView);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+
+        /*public void loadImage(String url, int index) {
+
+            LoadCacheAsyncTask task = new LoadCacheAsyncTask();
+            loadCacheTask.add(task);
+            task.execute(list.get(index));
+        }*/
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView)itemView.findViewById(R.id.item_image);
+        }
+    }
+
+    /*private class LoadCacheAsyncTask extends AsyncTask<String, Void, File>{
+
+        private int index;
+
+        public LoadCacheAsyncTask(int position) {
+            index = position;
+        }
+
+        @Override
+        protected File doInBackground(String... strings) {
+            File file = null;
+            try {
+                file = Glide.with(context)
+                           .load(strings[0])
+                           .downloadOnly(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+                           .get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            return file;
+        }
+
+        @Override
+        protected void onPostExecute(File file) {
+            if (file != null){
+
+            }else {
+                LoadImageAsyncTask task = new LoadImageAsyncTask(index);
+                loadImageTasks.add(task);
+                task.execute(file);
+            }
+
+            loadCacheTask.remove(this);
+        }
+    }
+
+    private class LoadImageAsyncTask extends AsyncTask<File, Void, DrawableTypeRequest<File>>{
+
+        private int index;
+
+        public LoadImageAsyncTask(int index) {
+            this.index = index;
+        }
+
+        @Override
+        protected DrawableTypeRequest<File> doInBackground(File... files) {
+
+            DrawableTypeRequest<File> type =  Glide.with(context).load(files[0]);
+
+            return type;
+        }
+
+        @Override
+        protected void onPostExecute(DrawableTypeRequest<File> bitmap) {
+            ImageView imageView = recyclerView.findViewWithTag(index);
+            bitmap.into(imageView);
+
+            loadImageTasks.remove(this);
+        }
+    }*/
+}
